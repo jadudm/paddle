35e5da7950b3d044f0b5fdda41b27948459d72f6
Matt Jadud <matt@jadud.com>
2018-12-14 18:16:05 -0500

    Yay. And, now my spacebar is sticking.

--- base.rkt
+++ base.rkt
@@ -5,3 +5,5 @@
 ;; I would like these globals somewhere else.
 (define world-rows (make-parameter 80))
 (define world-cols (make-parameter 80))
+
+(define pass 'pass)
--- boids.rkt
+++ boids.rkt
@@ -3,26 +3,32 @@
          "world.rkt"
          "forms.rkt")
 
-(world-cols 20)
-(world-rows 20)
-(tick-pause (/ 1 20))
+;; I'll make it a small world.
+;; The window is 600x600, so the number of
+;; columns essentially determines the resolution and
+;; size of the agents in the world.
+(world-cols 200)
+(world-rows 200)
 
+;; At full speed, it runs pretty fast. This determines
+;; the pause length between ticks of the world.
+(tick-pause 0)
+
+;; I'll have a breed of agents called fish.
 (define-breed fish fishes)
-(create-fishes 5)
+;; And I want this many fish.
+(create-fishes 2200)
 
+;; To set them up, I want to do the following.
+;; In this case, I do nothing.
 (define (setup)
-  'pass)
+  pass)
 
-(define (wiggle)
-  (if (> (random 100) 50)
-      (right (random 10))
-      (left (random 10))))
-
-(define (shimmer)
-  (set-color! (rgb (+ 128 (random 64))
-                   (+ 128 (random 64))
-                   (+ 128 (random 64)))))
-
+;; Perhaps this should be called 'act'?
+;; Perhaps not. Every time it is time to do stuff,
+;; the 'go' function is run. I will ask all the fishes
+;; to move forward one, to wiggle a bit, and to shimmer.
+;; The 'go' function is called over-and-over.
 (define (go)
   (ask-fishes
    (move 1)
@@ -30,7 +36,20 @@
    (shimmer)
    ))
 
-;;(run-world setup go)
+;; One of the things fish do is wiggle. I flip a
+;; coin, and depending on the result, I wiggle a bit
+;; to the left or to the right.
+(define (wiggle)
+  (if (> (random 100) 50)
+      (right (random 10))
+      (left (random 10))))
 
-(define stop
-  (run-world setup go))
+;; Shimmering means we randomly choose a new color.
+(define (shimmer)
+  (set-color! (rgb (+ 128 (random 64))
+                   (+ 128 (random 64))
+                   (+ 128 (random 64)))))
+
+
+;; Finally, we run the world.
+(run-world setup go)
