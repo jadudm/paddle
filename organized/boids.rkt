0ffaa549b43421e3beb6c605a2ed74f0c0ce827f
Matt Jadud <matt@jadud.com>
2018-12-18 10:21:52 -0500

    Improving comments.

--- organized/boids.rkt
+++ organized/boids.rkt
@@ -6,68 +6,65 @@
          "interface.rkt"
          )
 
-;; I'll make it a small world.
-;; The window is 600x600, so the number of
-;; columns essentially determines the resolution and
-;; size of the agents in the world.
+;; I must create the world first.
 (make-world 80 80 600 600)
-;; At full speed, it runs pretty fast. This determines
-;; the pause length between ticks of the world.
-;; (tick-pause (/ 1 60))
-;; I'll have a breed of agents called fish.
-(create-breed turtle turtles)
-;; And I want this many fish.
-(create turtles 30)
-;;(give turtles r-component)
 
-;; Patches
+;; The world does not always need patches.
+;; This world does.
 (create-patches)
 
-;; To set them up, I want to do the following.
-;; In this case, I do nothing.
+;; My creatures are called turtles.
+(create-breed turtle turtles)
+
+;; I want many turtles.
+(create turtles 300)
+
+;; I could slow things down.
+;; (tick-pause (/ 1 60))
+
+;; I want to set up my turtles.
 (define (setup)
+
+  ;; First, I want some sliders.
   (widgets (slider turtles 'r 1 255)
            (slider turtles 'g 1 255)
            (slider turtles 'b 1 255))
   
-  
+  ;; Then, I want to ask my turtles to scatter themselves
+  ;; randomly around the universe. They are mauve.
   (ask turtles
    (set xcor (random (get global world-cols)))
    (set ycor (random (get global world-rows)))
    (set color (rgb 255 0 128))
    )
-  
+
+  ;; Only a special few of my turtles are yellow.
   (ask (with turtles (member (get id) (range 5)))
        (set color (rgb 255 255 0)))
   )
 
-(define erasing? false)
-(define using-widgets? true)
+
+;; I want my turtles to go!
 (define (go)
+
+  ;; First, I ask all my turtles to wiggle and then move.
+  (ask turtles
+   (wiggle)
+   (move 1)
+   )
   
-  (when (zero? (modulo (ticker) 120))
-    (set! erasing? (not erasing?)))
-  
-  (ask (with turtles (and erasing? (not (member (get id) (range 5)))))
-       (set patch pcolor (rgb 0 0 0))
+  ;; Then, I ask all the not-special turtles to clear the patch
+  ;; they are on.
+  (ask (with turtles (not (member (get id) (range 5))))
        (set patch dirty? false))
 
-  (if using-widgets?
-      (ask (with turtles (member (get id) (range 5)))
+  ;; Finally,  I ask the special turtles to use the slider values
+  ;; to set their patches to a pretty color.
+  (ask (with turtles (member (get id) (range 5)))
            (set patch pcolor (rgb (random (get r)) 
                                   (random (get g))
                                   (random (get b))
                                   )))
-
-      (ask (with turtles (member (get id) (range 5)))
-           (set patch pcolor (rgb (+ 64 (random 128))
-                                  (+ 64 (random 128))
-                                  (+ 64 (random 128))
-                                  ))))
-
-  (ask turtles
-   (move 1)
-   (wiggle))
   )
 
 ;; One of the things fish do is wiggle. I flip a
@@ -79,11 +76,12 @@
       (left (random 10))))
 
 ;; Shimmering means we randomly choose a new color.
+;; None of my turtles currently shimmer.
 (define (shimmer)
   (set color (rgb (+ 128 (random 64))
                   (+ 128 (random 64))
                   (+ 128 (random 64)))))
 
 
-;; Finally, we run the world.
+;; Finally, I run the world.
 (run-world setup go)
