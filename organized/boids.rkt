dd30f894edf9e7135d3cd8a31a4139eb71bd33d7
Matt Jadud <matt@jadud.com>
2018-12-18 08:01:05 -0500

    Small changes to boids.

--- organized/boids.rkt
+++ organized/boids.rkt
@@ -11,7 +11,7 @@
 (make-world 100 100 400 400)
 ;; At full speed, it runs pretty fast. This determines
 ;; the pause length between ticks of the world.
-;(tick-pause (/ 1 60))
+;; (tick-pause (/ 1 60))
 ;; I'll have a breed of agents called fish.
 (create-breed turtle turtles)
 ;; And I want this many fish.
@@ -26,46 +26,31 @@
   (ask turtles
    (set xcor (random (get global world-cols)))
    (set ycor (random (get global world-rows)))
-   (set color (rgb 255 0 0))
+   (set color (rgb 255 0 128))
    )
   (ask (with turtles (member (get id) (range 5)))
        (set color (rgb 255 255 0)))
   )
-  
+
+(define erasing? false)
 (define (go)
-  #;(when (zero? (modulo (ticker) 120))
-    (printf "dp: ~a~n" (hash-count (agentset-agents (hash-ref agentsets 'dirty-patches)))))
   
   (ask turtles
    (move 1)
    (wiggle))
 
-  (ask (other turtles)
+  (when (zero? (modulo (ticker) 120))
+    (set! erasing? (not erasing?)))
+  
+  (ask (with turtles (and erasing? (not (member (get id) (range 5)))))
        (set patch pcolor (rgb 0 0 0))
        (set patch dirty? false))
   
   (ask (with turtles (member (get id) (range 5)))
        (set patch pcolor (rgb (+ 64 (random 128))
                               (+ 64 (random 128))
-                              (+ 64 (random 128))))
-       )
+                              (+ 64 (random 128)))))
   )
-
-
-  #;(ask patches
-         (set dirty? true)
-         (set pcolor (rgb (exact-floor (* 255 (/ (get pxcor) (get world-cols))))
-                          (exact-floor (* 255 (/ (get pycor) (get world-rows))))
-                          0
-                          )))
-#;(set color (rgb (modulo (+ 32 (random 32) (ticker)) 256)
-                       255
-                       (modulo (ticker) 256)))
-
-#;(ask patches
-       (set pcolor (rgb (+ 64 (random 64))
-                        (+ 64 (random 64))
-                        (+ 64 (random 64)))))
 
 ;; One of the things fish do is wiggle. I flip a
 ;; coin, and depending on the result, I wiggle a bit
